882ea679b5ed71e242cfb201258b43c77bf11e19
Daniel <danielajib10@gmail.com>
2026-02-09 19:41:16 +0100

    v

--- src/Commands/GoCommand.java
+++ src/Commands/GoCommand.java
@@ -15,23 +15,7 @@ public class GoCommand implements Command {
 
     public void execute(String[] parameters) {
         if (parameters.length == 0) {
-            System.out.println("Musíš zadat směr (např. jdi sever).");
-            return;
-        }
-
-        String direction = parameters[0].toLowerCase();
-        Room currentRoom = game.getPlayer().getCurrentRoom();
-        Room nextRoom = currentRoom.getExit(direction);
-        if (nextRoom == null) {
-            System.out.println("Tímto směrem se nedá jít.");
-            return;
-        }
-        if (nextRoom.getName().equalsIgnoreCase("Depozitar")
-                && game.getPlayer().getInventory().findItemByName("Univerzalni klic") == null) {
-            System.out.println("Depozitář je zamčený. Potřebuješ univerzální klíč.");
-            return;
-        }
-        if (game.movePlayer(direction)) {
+   layer(direction)) {
             game.getMap();
             game.describeCurrentRoom();
         }
