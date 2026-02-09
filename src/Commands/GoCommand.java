2395b8583d307a4be08414268452e8a848f04390
Daniel <danielajib10@gmail.com>
2026-02-09 19:41:48 +0100

    kdyz chce hrac jit do depozitare tak u sebe musi mit univerzalni klic

--- src/Commands/GoCommand.java
+++ src/Commands/GoCommand.java
@@ -15,7 +15,23 @@ public class GoCommand implements Command {
 
     public void execute(String[] parameters) {
         if (parameters.length == 0) {
-   layer(direction)) {
+            System.out.println("Musíš zadat směr (např. jdi sever).");
+            return;
+        }
+
+        String direction = parameters[0].toLowerCase();
+        Room currentRoom = game.getPlayer().getCurrentRoom();
+        Room nextRoom = currentRoom.getExit(direction);
+        if (nextRoom == null) {
+            System.out.println("Tímto směrem se nedá jít.");
+            return;
+        }
+        if (nextRoom.getName().equalsIgnoreCase("Depozitar")
+                && game.getPlayer().getInventory().findItemByName("Univerzalni klic") == null) {
+            System.out.println("Depozitář je zamčený. Potřebuješ univerzální klíč.");
+            return;
+        }
+        if (game.movePlayer(direction)) {
             game.getMap();
             game.describeCurrentRoom();
         }
