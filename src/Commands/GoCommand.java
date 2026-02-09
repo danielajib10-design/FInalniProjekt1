59e6f6a8b59b83b86610ea3249fd0cbe847328dc
Daniel <danielajib10@gmail.com>
2026-02-09 19:38:23 +0100

    kdyz chce jit hrac do depozitare tak u sebe musi mit univerzalni klic

--- src/Commands/GoCommand.java
+++ src/Commands/GoCommand.java
@@ -1,6 +1,7 @@
 package Commands;
 
 import Core.Game;
+import Core.Room;
 
 public class GoCommand implements Command {
 
@@ -19,11 +20,20 @@ public class GoCommand implements Command {
         }
 
         String direction = parameters[0].toLowerCase();
+        Room currentRoom = game.getPlayer().getCurrentRoom();
+        Room nextRoom = currentRoom.getExit(direction);
+        if (nextRoom == null) {
+            System.out.println("Tímto směrem se nedá jít.");
+            return;
+        }
+        if (nextRoom.getName().equalsIgnoreCase("Depozitar")
+                && game.getPlayer().getInventory().findItemByName("Univerzalni klic") == null) {
+            System.out.println("Depozitář je zamčený. Potřebuješ univerzální klíč.");
+            return;
+        }
         if (game.movePlayer(direction)) {
             game.getMap();
             game.describeCurrentRoom();
-        } else {
-            System.out.println("Tímto směrem se nedá jít.");
         }
     }
 }
