5b320da6b58f631dfb27e1e2002047c19adde406
Daniel <danielajib10@gmail.com>
2026-02-09 19:40:56 +0100

    pred pouzitim musi hrac opravit kavovar a ten opravy tim ze ho pouzije a potom do nej muze dat minci a dostane za to horkou kavu

--- src/Commands/UseCommand.java
+++ src/Commands/UseCommand.java
@@ -1,8 +1,6 @@
 package Commands;
 
-import Core.Game;
-import Core.Inventory;
-import Core.Item;
+import Core.*;
 
 public class UseCommand implements Command {
 
@@ -19,9 +17,39 @@ public class UseCommand implements Command {
         }
 
         String itemName = String.join(" ", parameters);
+        Player player = game.getPlayer();
+        Room room = player.getCurrentRoom();
         Inventory inventory = game.getPlayer().getInventory();
         Item item = inventory.findItemByName(itemName);
+        if (item != null && item.getName().equalsIgnoreCase("Mince") && room.getName().equalsIgnoreCase("Bufet")) {
+            if (!player.isCoffeeMachineRepaired()) {
+                System.out.println("Kávovar je rozbitý. Nejprve ho musíš opravit.");
+                return;
+            }
+            Item coffee = new Item(
+                    "Horka kava",
+                    "Předmět, který slouží jako prostředek k získání klíče od pana Nováka.",
+                    true
+            );
+            if (!inventory.addItem(coffee)) {
+                System.out.println("Inventář je plný. Nejprve si uvolni místo na kávu.");
+                return;
+            }
+            inventory.removeItem(item);
+            System.out.println("Vhodil jsi minci do kávovaru a získal horkou kávu.");
+            return;
+        }
         if (item == null) {
+            Item roomItem = room.findItemByName(itemName);
+            if (roomItem != null && roomItem.getName().equalsIgnoreCase("Kavovar")) {
+                if (player.isCoffeeMachineRepaired()) {
+                    System.out.println("Kávovar je opravený.");
+                    return;
+                }
+                player.setCoffeeMachineRepaired(true);
+                System.out.println("Opravil jsi kávovar. Teď můžeš použít minci.");
+                return;
+            }
             System.out.println("Tento předmět v inventáři nemáš.");
             return;
         }
